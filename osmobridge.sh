d1f121c74b5ae7294b8979962e9f58c24994c67e
Mike Neir <mike.neir@gmail.com>
2016-10-24 19:21:58 -0400

    It's good to actually put the DROPs in there instead of the testing ACCEPTs.

--- osmobridge.sh
+++ osmobridge.sh
@@ -161,8 +161,8 @@ else
 	iptables -t nat -A POSTROUTING -o eth1 -s 192.168.2.0/24 -j MASQUERADE
 
 	# make security great again
-	iptables -P INPUT ACCEPT
-	iptables -P FORWARD ACCEPT
+	iptables -P INPUT DROP
+	iptables -P FORWARD DROP
 
 
 	service hostapd start
